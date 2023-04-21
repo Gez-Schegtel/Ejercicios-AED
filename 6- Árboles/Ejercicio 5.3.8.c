fd54a2d4858665c8655e95a19a9a94ce42c7871e
Juan Ignacio Velazco Gez Schegtel <Gez_Schegtel@tutanota.com>
2023-04-21 17:27:25 -0300

    Update Ejercicio 5.3.8.c

--- 6- Árboles/Ejercicio 5.3.8.c
+++ 6- Árboles/Ejercicio 5.3.8.c
@@ -2,20 +2,19 @@ Ejercicio 5.3.8
 Escribir una función recursiva que encuentre la altura de un árbol binario.
 
 funcion fr(p:puntero a nodo):entero  ES
-ambiente 
-    r1,r2: entero 
+    ambiente 
+        r1,r2: entero 
 
-proceso 
-    si (p = nil) entonces
-        fr:=0
-    sino 
-        r1:= fr(*p.izq)
-        r2:= fr(*p.der)
-        si (r1 > r2) entonces
-            fr:= 1 + r1 
+    proceso 
+        si (p = nil) entonces
+            fr:=0
         sino 
-            fr:= 1 + r2
-        fin_si
-    fin_si 
-
+            r1:= fr(*p.izq)
+            r2:= fr(*p.der)
+            si (r1 > r2) entonces
+                fr:= 1 + r1 
+            sino 
+                fr:= 1 + r2
+            fin_si
+        fin_si 
 fin_funcion
